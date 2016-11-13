6c9ab6793019b79f2facf3116f0d45d2a43e4c4e
Meatlf <454779758@qq.com>
2016-11-13 19:07:59 +0800

    Delete kernel.h

--- kernel.h
+++
@@ -1,8 +0,0 @@
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <stdint.h>
-#include "ModP.h"
-
-__global__ void dotMul_kernel (uint64_t* x, uint64_t* y, uint64_t* output, int length);
-
-void dotMul(uint64_t* x, uint64_t*y, uint64_t* output, int length);
